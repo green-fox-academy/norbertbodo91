60001902f4f346b790aa25fede197306c124307c
norbert.bodo91 <norbert.bodo91@gmail.com>
2018-03-02 15:13:31 +0100

    BrutalSquareFinal

---
+++ week-03/day-5/DrawBrutalSquare.cs
@@ -0,0 +1,55 @@
+using System.Windows;
+
+namespace MindUck
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var foxDraw = new FoxDraw(canvas);
+
+            const float margin = 10;
+            Rect rect = new Rect(
+                margin, margin,
+                canvas.Width - 2 * margin,
+                canvas.Height - 2 * margin);
+
+            int level = 4;
+
+            DrawBrutalSquare(foxDraw,level,rect);
+           
+        }
+        public void DrawBrutalSquare(FoxDraw foxDraw, int level, Rect rect)
+        {
+            
+            if (level == 0)
+            {
+                foxDraw.DrawRectangle(rect.X,rect.Y,rect.Width,rect.Height); 
+            }
+            else
+            {
+                float width = (float)rect.Width / 3f;
+                float x0 = (float)rect.Left;
+                float x1 = x0 + width;
+                float x2 = x0 + width * 2f;
+
+                float height = (float)rect.Height / 3f;
+                float y0 = (float)rect.Top;
+                float y1 = y0 + height;
+                float y2 = y0 + height * 2f;
+
+                DrawBrutalSquare(foxDraw, level - 1, new Rect(x0, y0, width, height));
+                DrawBrutalSquare(foxDraw, level - 1, new Rect(x1, y0, width, height));
+                DrawBrutalSquare(foxDraw, level - 1, new Rect(x2, y0, width, height));
+                DrawBrutalSquare(foxDraw, level - 1, new Rect(x0, y1, width, height));
+                DrawBrutalSquare(foxDraw, level - 1, new Rect(x2, y1, width, height));
+                DrawBrutalSquare(foxDraw, level - 1, new Rect(x0, y2, width, height));
+                DrawBrutalSquare(foxDraw, level - 1, new Rect(x1, y2, width, height));
+                DrawBrutalSquare(foxDraw, level - 1, new Rect(x2, y2, width, height));
+            }
+        }
+    }
+}
