e03fa4eea8787e30211f3d1eb383581729633f2d
norbert.bodo91 <norbert.bodo91@gmail.com>
2018-04-05 12:32:09 +0200

    CounterWithOutStatic

---
+++ week-07/day-04/CounterWebApp/Controllers/NumberCounterController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CounterWebApp.Service;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CounterWebApp.Controllers
+{
+    
+    public class NumberCounterController : Controller
+    {
+        private readonly ICountable Countable;
+
+        public NumberCounterController(ICountable countable)
+        {
+            Countable = countable;
+        }
+
+        [HttpGet]
+        [Route("Counter")]
+        public IActionResult Counter()
+        {
+            return View(Countable);
+        }
+        [HttpPost]
+        [Route("Increase")]
+        public IActionResult Incease()
+        {            
+            Countable.IncreaseNumber();
+            return RedirectToAction("Counter");;
+        }
+    }
+}
---
+++ week-07/day-04/CounterWebApp/Models/NumberCounter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CounterWebApp.Service;
+
+namespace CounterWebApp.Models
+{
+    public class NumberCounter : ICountable
+    {
+        public int Number { get; set; }
+
+        public NumberCounter(int number = 0)
+        {
+            Number = number;
+        }
+        public void IncreaseNumber()
+        {
+            Number++;
+        }
+    }
+}
