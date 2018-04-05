4235e2b46f2c8ed367f3615de206d63637be529f
norbert.bodo91 <norbert.bodo91@gmail.com>
2018-04-05 15:29:23 +0200

    AnagramWithBeautfilCheckedHtml...

---
+++ week-07/day-04/AnagramCheckWebApp/Models/AnagramCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using AnagramCheckWebApp.Services;
+
+namespace AnagramCheckWebApp.Models
+{
+    public class AnagramCheck : IAnagram
+    {
+        public string CheckIfAnagramOrNot(string firstWord, string secondWord)
+        {
+            if (firstWord == null || secondWord == null)
+            {
+                return "Incorrect input...";
+            }
+            if (String.Concat(firstWord.OrderBy(x => x)).ToLower().Equals
+                (String.Concat(secondWord.OrderBy(x => x)).ToLower()) && firstWord != secondWord)
+            {
+                return "Words are anagram !!";
+            }
+            else
+            {
+                return "Sorry your words are not anagram !!";
+            }
+        }
+    }
+}
---
+++ week-07/day-04/AnagramCheckWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AnagramCheckWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
