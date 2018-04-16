30af5e4cc147eebea38adc30d046965d1ad7d450
norbert.bodo91 <norbert.bodo91@gmail.com>
2018-04-16 12:02:58 +0200

    frontedAdded

---
+++ week-09/day-01/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+    }
+}
